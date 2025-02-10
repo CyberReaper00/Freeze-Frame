21faecc43334393a4fd4353e952c7a52349ad5a1
CyberReaper00 <nahyanl63@gmail.com>
2025-02-10 16:27:22 +0500

    did some tests

--- ff.c
+++ ff.c
@@ -1,2 +1,7 @@
+#include <stdio.h>
 #include <windows.h>
 
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    MessageBox(NULL, "Hello Windows!", "This is a title", MB_OK);
+    return 0;
+}
